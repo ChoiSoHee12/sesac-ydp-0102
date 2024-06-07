b83b6bfd0c8ff9811e999f9f6b6e8ba4254514da
ChoiSoHee12 <chlthgml0102@gmail.com>
2024-06-07 19:00:26 +0900

    node.js 보충

--- 06-NPM/app2.js
+++ 06-NPM/app2.js
@@ -1,7 +1,17 @@
 const http = require('http');
 http.createServer(function(req, res) {
-    res.writeHead(200, {'content-type': 'text/plain'})
-    res.end(`<h2> hello. 최소희</h2>`)
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.end(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h2>최소희</h2>
+    </body>
+    </html>`)
 
 }).listen(3000, () => {
     console.log('서버 실행')
