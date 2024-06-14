178275d2655ac08937027f49c95f39c101696895
ChoiSoHee12 <chlthgml0102@gmail.com>
2024-06-14 17:08:21 +0900

    callback/promise

---
+++ LECTURE/11-async/index2실습.js
@@ -0,0 +1,32 @@
+function call(name) {
+    return new Promise (function(resolve) {
+        setTimeout(function() {
+            console.log(name);
+            const result = name;
+            resolve(result)
+        }, 1000);
+    })   
+}
+
+function back(cb) {
+    setTimeout(function() {
+        console.log('back');
+        cb('back');
+    }, 1000)
+}
+
+function hell(cb) {
+    setTimeout(function() {
+        cb('callback hell');
+    }, 1000);
+}
+
+call('kim', function (name) {
+    console.log(name + '반가워');
+    back(function (txt) {
+        console.log(txt + '을 실행했구나');
+        hell(function (message) {
+            console.log('여기는' + message);
+        });
+    });
+});
