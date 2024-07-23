50bca6faca541d225b07e1742bd48e795d8698f0
GitHub ChoiSoHee12 <chlthgml0102@gmail.com>
2024-07-24 00:45:46 +0900

    안 되던것들 실험(로고,입력창 등등)

--- LECTURE/09-STATIC/app.js
+++ LECTURE/09-STATIC/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
+//app.use(express.static('public'))
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static('public'))
+// 정적 파일 제공 설정
+app.use(express.static(path.join(__dirname, 'public')));;
+
 app.get('/', (req,res)=>{
     res.render('index', {title :'Home'})
 })
@@ -13,6 +20,9 @@ app.get('/create', (req,res)=>{
     res.render('create', {title :'Create'})
 })
 
+app.get('/index2', (req,res)=>{
+    res.render('index2', {title :'index2'})
+})
 //지정된 경로외 경로일 때
 app.get((req,res)=>{
     res.status(404).rander('404')
