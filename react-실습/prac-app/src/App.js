9d83d87006a12530af7a074bb06ebd9ed3432fcd
ChoiSoHee12 <chlthgml0102@gmail.com>
2024-08-05 14:05:50 +0900

    실습

---
+++ react-실습/prac-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App () {
+  const name = '뽀리';
+const animal = 'Pomeranian';
+
+}
+
+return (
+  <h2>제 반려 동물의 이름은 {name}입니다. {name}은 {animal}입니다. </h2>
+
+ 
+)
+
+export default App;
