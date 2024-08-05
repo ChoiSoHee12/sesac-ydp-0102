77ae06f1167f195641afc9fe00c1894476c32856
ChoiSoHee12 <chlthgml0102@gmail.com>
2024-08-05 18:36:14 +0900

    실습 포함

--- LECTURE/27-event/src/App.js
+++ LECTURE/27-event/src/App.js
@@ -13,6 +13,8 @@ function App() {
       <hr />
 
       <Counter/>
+
+      <hr />
     </div>
   );
 }
---
+++ LECTURE/27-event/src/ex/HandlerEx.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+export default class HandlerEx extends Component {
+
+    constructor(props) {
+    super(props);
+     this.state = {
+        
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const [color, setColor] = useStste('black');
+     return(
+        <div>
+            <button onClick={onclickRed}>빨간색</button>
+            <button onClick={onclickBlue}>파란색</button>
+        </div>
+     )
+    }
+}
